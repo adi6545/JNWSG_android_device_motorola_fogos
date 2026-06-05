638dfeb50a23e9ff276598ebd63d101259c32eb5
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-06-05 14:55:31 +0530

    Update lineage_fogos.mk

--- lineage_fogos.mk
+++ lineage_fogos.mk
@@ -17,6 +17,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Lunaris-Flags
 WITH_GMS := true
 WITH_BCR := true
+TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_NAME := lineage_fogos
 PRODUCT_DEVICE := fogos
