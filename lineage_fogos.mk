756d8f21284560196c7382e22050d686341202f6
Aditya <96648902+adi6545@users.noreply.github.com>
2026-08-04 14:44:10 +0530

    Update lineage_fogos.mk

--- lineage_fogos.mk
+++ lineage_fogos.mk
@@ -16,14 +16,13 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Lunaris-Flags
 WITH_GMS := true
-WITH_BCR := true
 TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_NAME := lineage_fogos
 PRODUCT_DEVICE := fogos
-PRODUCT_MANUFACTURER := motorola
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g34 5G
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G34
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
